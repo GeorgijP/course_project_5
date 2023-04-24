2f62de5e4fb99eee2734b7ea432c464dcd38301e
GeorgijP <georgij.puzanov@inbox.ru>
2023-04-24 19:26:55 +0400

    Написал README.md

--- classes/class_DBManager.py
+++ classes/class_DBManager.py
@@ -8,50 +8,62 @@ class DBManager():
         """ Получает список всех компаний и количество вакансий у каждой компании."""
 
         connection = psycopg2.connect(**config_hh())
-        with connection.cursor() as cur:
-            cur.execute("SELECT name_company, number_vacancies FROM company")
-            cur = cur.fetchall()
-            for i in range(len(cur)):
-                print(f"Название компании: {cur[i][0]}, количество вакансий: {cur[i][1]}\n")
+        try:
+            with connection.cursor() as cur:
+                cur.execute("SELECT name_company, number_vacancies FROM company")
+                cur = cur.fetchall()
+                for i in range(len(cur)):
+                    print(f"Название компании: {cur[i][0]}, количество вакансий: {cur[i][1]}\n")
+        finally:
+            connection.close()
 
     def get_all_vacancies(self):
         """Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию."""
 
         connection = psycopg2.connect(**config_hh())
-        with connection.cursor() as cur:
-            cur.execute(
-                "SELECT name_company, name_vacancy, salary, url_vacancy FROM vacancies INNER JOIN company USING(id_company)"
-            )
-            cur = cur.fetchall()
-            for i in range(len(cur)):
-                if cur[i][2] is None:
-                    print(f"Название компании: {cur[i][0]}\nНазвания вакансии: {cur[i][1]}\nЗарплата не указана\nСсылка на вакансию: {cur[i][3]}\n")
-            else:
-                print(f"Название компании: {cur[i][0]}\nНазвания вакансии: {cur[i][1]}\nЗарплата: {cur[i][2]} рублей\nСсылка на вакансию: {cur[i][3]}\n")
-
+        try:
+            with connection.cursor() as cur:
+                cur.execute(
+                    "SELECT name_company, name_vacancy, salary, url_vacancy FROM vacancies INNER JOIN company USING(id_company)"
+                )
+                cur = cur.fetchall()
+                for i in range(len(cur)):
+                    if cur[i][2] is None:
+                        print(
+                            f"Название компании: {cur[i][0]}\nНазвания вакансии: {cur[i][1]}\nЗарплата не указана\nСсылка на вакансию: {cur[i][3]}\n")
+                else:
+                    print(
+                        f"Название компании: {cur[i][0]}\nНазвания вакансии: {cur[i][1]}\nЗарплата: {cur[i][2]} рублей\nСсылка на вакансию: {cur[i][3]}\n")
+        finally:
+            connection.close()
 
     def get_avg_salary(self):
         """Получает среднюю зарплату по вакансиям."""
 
         connection = psycopg2.connect(**config_hh())
-        with connection.cursor() as cur:
-            cur.execute("SELECT ROUND(AVG(salary)) FROM vacancies")
-            cur = cur.fetchone()
-            print(f"Средняя зарплата: {cur[0]} рублей\n")
-
+        try:
+            with connection.cursor() as cur:
+                cur.execute("SELECT ROUND(AVG(salary)) FROM vacancies")
+                cur = cur.fetchone()
+                print(f"Средняя зарплата: {cur[0]} рублей\n")
+        finally:
+            connection.close()
 
     def get_vacancies_with_higher_salary(self):
         """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
 
         connection = psycopg2.connect(**config_hh())
-        with connection.cursor() as cur:
-            cur.execute(
-                "SELECT name_company, name_vacancy, salary, url_vacancy FROM vacancies INNER JOIN company USING(id_company) WHERE salary > (SELECT ROUND(AVG(salary)) FROM vacancies)"
-            )
-            cur = cur.fetchall()
-            for i in range(len(cur)):
-                print(f"Название компании: {cur[i][0]}\nНазвания вакансии: {cur[i][1]}\nЗарплата: {cur[i][2]} рублей\nСсылка на вакансию: {cur[i][3]}\n")
-
+        try:
+            with connection.cursor() as cur:
+                cur.execute(
+                    "SELECT name_company, name_vacancy, salary, url_vacancy FROM vacancies INNER JOIN company USING(id_company) WHERE salary > (SELECT ROUND(AVG(salary)) FROM vacancies)"
+                )
+                cur = cur.fetchall()
+                for i in range(len(cur)):
+                    print(
+                        f"Название компании: {cur[i][0]}\nНазвания вакансии: {cur[i][1]}\nЗарплата: {cur[i][2]} рублей\nСсылка на вакансию: {cur[i][3]}\n")
+        finally:
+            connection.close()
 
     def get_vacancies_with_keyword(self):
         """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”."""
@@ -59,10 +71,14 @@ class DBManager():
         key_word = input('Введите ключевое слово: ')
 
         connection = psycopg2.connect(**config_hh())
-        with connection.cursor() as cur:
-            cur.execute(
-                f"SELECT name_company, name_vacancy, salary, url_vacancy FROM vacancies INNER JOIN company USING(id_company) WHERE name_vacancy LIKE '%{key_word}%'"
-            )
-            cur = cur.fetchall()
-            for i in range(len(cur)):
-                print(f"Название компании: {cur[i][0]}\nНазвания вакансии: {cur[i][1]}\nЗарплата: {cur[i][2]} рублей\nСсылка на вакансию: {cur[i][3]}\n")
+        try:
+            with connection.cursor() as cur:
+                cur.execute(
+                    f"SELECT name_company, name_vacancy, salary, url_vacancy FROM vacancies INNER JOIN company USING(id_company) WHERE name_vacancy LIKE '%{key_word}%'"
+                )
+                cur = cur.fetchall()
+                for i in range(len(cur)):
+                    print(
+                        f"Название компании: {cur[i][0]}\nНазвания вакансии: {cur[i][1]}\nЗарплата: {cur[i][2]} рублей\nСсылка на вакансию: {cur[i][3]}\n")
+        finally:
+            connection.close()
